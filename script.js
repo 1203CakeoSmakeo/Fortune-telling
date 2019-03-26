51318b66c309c4ee7b439e685ccf63f04b5faddd
1203CakeoSmakeo <43444272+1203CakeoSmakeo@users.noreply.github.com>
2019-03-26 09:33:30 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,47 @@
+//I'm going to ask the quiz testers every thing that has to do with there lives, how they spend there money, and how they live there life, what they have in there personal lives, and what got them to where they are now and also the predictimit there in know technically the questions im going to ask them is to see if they are weaboos or not.
+//Question 1:Do you have a magical girl doll
+//Question 2:Have you spent most of your life watching anime instead of any other shows on tv
+//Question 3:Do you weight around 300 pounds
+//Question 4:Do you spend over 100$ on anime figures
+//Question 5:Do you sit on your knees
+//Question 6:Is japanese food your favorite
+//Question 7:Do you fight using anime technique
+//Question 8:Do you watch anime
+//Question 9:Do you live in your parents basement                            
+
+$("button").click(function() {
+    var Tyrant= $("#Tyrant").val();
+    console.log("Tyrant");
+});
+
+$("button").click(function() {
+    var Pika= $("#Pika").val();
+    console.log("Pika");
+});
+
+
+$("button").click(function() {
+    var Tea= $("#Tea").val();
+    console.log("Tea");
+});
+
+
+$("button").click(function() {
+    var PUBG= $("#PUBG").val();
+    console.log("PUBG");
+});
+
+$("button").click(function() {
+    var Anime= $("#Anime").val();
+    console.log("Anime");
+});
+
+$("button").click(function() {
+    var Saitama= $("#Saitama").val();
+    console.log("Saitama");
+});
+
+$("button").click(function() {
+    var Yujiro= $("#Yujiro").val();
+    console.log("Yujiro");
+});
